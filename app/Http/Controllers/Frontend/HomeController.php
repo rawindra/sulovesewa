8e1d47b13eefc6347c06a598ba70f0aee174267e
Bunny <bunnycdy@gmail.com>
2024-07-15 15:04:01 +0545

    products returned in home page

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -12,7 +13,8 @@ class HomeController extends Controller
     public function home() {
         $categories = Category::all();
         return Inertia::render('Front/Home', [
-            'categories' => $categories
+            'categories' => $categories,
+            'products' => Product::paginate(10)
         ]);
     }
 }
