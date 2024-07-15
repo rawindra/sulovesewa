493c373b3d59e60fc5651118a0da7eedf1cf49a1
Bunny <bunnycdy@gmail.com>
2024-07-15 13:38:31 +0545

    `home` function added in the `HomeController` to return `Front\Home` page

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class HomeController extends Controller
 {
-    //
+    public function home() {
+        Inertia::render('Front/Home');
+    }
 }
