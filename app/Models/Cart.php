767459a1cfef1b8ef9cf7262ea01be96d5445c2a
Bunny <bunnycdy@gmail.com>
2024-07-15 23:01:49 +0545

    user() and product() relation added in Cart

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cart extends Model
 {
@@ -14,4 +15,23 @@ class Cart extends Model
         'product_id',
         'quantity',
     ];
+
+    /**
+     * Get the user that owns the Cart
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    /**
+     * Get the product that owns the Cart
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
 }
