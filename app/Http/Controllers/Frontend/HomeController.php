bf3ba09c67c0b5ff68bb46d4d69813a066586e05
Bunny <bunnycdy@gmail.com>
2024-07-16 11:49:31 +0545

    cartItems return to home page

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -6,33 +6,39 @@ use App\Http\Controllers\Controller;
 use App\Models\Cart;
 use App\Models\Category;
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class HomeController extends Controller
 {
     public function home()
     {
-        $categories = Category::whereNull('parent_id')
-            ->with('children')
-            ->get();
+        $categories = Category::whereNull("parent_id")->with("children")->get();
 
-        return Inertia::render('Front/Home', [
-            'categories' => $categories,
-            'products' => Product::paginate(10)
+        $cartItems = [];
+        if (Auth::check()) {
+            $cartItems = Cart::where("user_id", auth()->user()->id)->get();
+        }
+
+        return Inertia::render("Front/Home", [
+            "categories" => $categories,
+            "products" => Product::paginate(10),
+            "cartItems" => $cartItems,
         ]);
     }
 
     public function product(Product $product)
     {
-        return Inertia::render('Front/ProductShow', [
-            'product' => $product->load('category', 'brand'),
+        return Inertia::render("Front/ProductShow", [
+            "product" => $product->load("category", "brand"),
         ]);
     }
 
     public function cart(Request $request)
     {
-        
-        $carts = Cart::where('user_id', auth()->user()->id)->where('product_id', $request->product_id)->first();
+        $carts = Cart::where("user_id", auth()->user()->id)
+            ->where("product_id", $request->product_id)
+            ->first();
         if ($carts) {
             $cart = Cart::find($carts->id);
             $cart->user_id = $request->user()->id;
@@ -40,7 +46,7 @@ class HomeController extends Controller
             $cart->quantity = $cart->quantity + $request->quantity;
             $cart->save();
         } else {
-            $cart = new Cart;
+            $cart = new Cart();
             $cart->user_id = $request->user()->id;
             $cart->product_id = $request->product_id;
             $cart->quantity = $request->quantity;
