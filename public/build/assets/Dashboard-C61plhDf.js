a437d927b5e78b68d0354deb7c915c20fc6c570a
Bunny <bunnycdy@gmail.com>
2024-07-16 20:45:43 +0545

    build new

---
+++ public/build/assets/Dashboard-C61plhDf.js
@@ -0,0 +1,1 @@
+import{j as r,Y as o}from"./app-BM7AwQ8q.js";import{A as e}from"./AuthenticatedLayout-BHOzn-kr.js";import"./index-mZEDTe4X.js";import"./index-nsjdkozt.js";function n({auth:t}){return r.jsxs(e,{user:t.user,children:[r.jsx(o,{title:"Dashboard"}),r.jsx("p",{children:"You're logged in!"})]})}export{n as default};
