97d214cf0fa8bb990421f7288e6d4912b37209e6
Bunny <bunnycdy@gmail.com>
2024-07-16 12:51:02 +0545

    Drawer implemented to list cartItems

--- resources/js/Components/partials/Navbar.jsx
+++ resources/js/Components/partials/Navbar.jsx
@@ -1,48 +1,90 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import { Link, usePage } from '@inertiajs/react'
-import { Badge } from 'antd'
+import { Link, usePage } from "@inertiajs/react";
+import { Badge, Drawer } from "antd";
 
 import { FaPowerOff, FaSearch } from "react-icons/fa";
 import { FaCartArrowDown } from "react-icons/fa";
 
+// const cartItems = [];
 
-const Navbar = () => {
-
-    const auth = usePage().props.auth
-
-    return (
-        <div className="p-6  bg-orange-500">
-            <div className='flex items-center justify-between'>
-                <Link className="text-white font-bold text-2xl" href="/">
-                    SuLoveSewa
-                </Link>
-                <div className='hidden md:block relative'>
-                    <FaSearch size={20} className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-300" />
-                    <input type="search" className="w-[300px] md:w-[400px] focus:outline-none p-2 pl-10 text-sm dark:text-gray-900 rounded-lg" name="search" placeholder="Search Products..." />
-                </div>
-                <div className="flex items-center">
-                    {auth.user ? (
-                        <Link as='button' href={route('logout')} className='text-green-600 mr-3' method='post'>
-                            <FaPowerOff size={20} />
-                        </Link>
-                    ) : <a href='/login/google'>
-                        <button className="font-medium bg-white text-sm text-orange-500 py-2 px-10 border rounded-md mx-5 ">Login</button>
-                    </a>}
-
-                    <Link className='pt-2'>
-                        <Badge count={1}>
-                            <FaCartArrowDown size={30} className='text-white' />
-                        </Badge>
-                    </Link>
-                </div>
-            </div>
-            <div className='block md:hidden relative mt-2'>
-                <FaSearch size={20} className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-300" />
-                <input type="search" className="w-full focus:outline-none p-2 pl-10 text-sm dark:text-gray-900 rounded-lg" name="search" placeholder="Search Products..." />
-            </div>
+const Navbar = ({ cartItems }) => {
+  const auth = usePage().props.auth;
+  const [open, setOpen] = useState(false);
+  const showDrawer = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div className="p-6  bg-orange-500">
+      <div className="flex items-center justify-between">
+        <Link className="text-white font-bold text-2xl" href="/">
+          SuLoveSewa
+        </Link>
+        <div className="hidden md:block relative">
+          <FaSearch
+            size={20}
+            className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-300"
+          />
+          <input
+            type="search"
+            className="w-[300px] md:w-[400px] focus:outline-none p-2 pl-10 text-sm dark:text-gray-900 rounded-lg"
+            name="search"
+            placeholder="Search Products..."
+          />
         </div>
-    )
-}
+        <div className="flex items-center">
+          {auth.user ? (
+            <Link
+              as="button"
+              href={route("logout")}
+              className="text-green-600 mr-3"
+              method="post"
+            >
+              <FaPowerOff size={20} />
+            </Link>
+          ) : (
+            <a href="/login/google">
+              <button className="font-medium bg-white text-sm text-orange-500 py-2 px-10 border rounded-md mx-5 ">
+                Login
+              </button>
+            </a>
+          )}
 
-export default Navbar
+          <button className="pt-2" onClick={showDrawer}>
+            <Badge count={cartItems.length}>
+              <FaCartArrowDown size={30} className="text-white" />
+            </Badge>
+          </button>
+        </div>
+      </div>
+      <div className="block md:hidden relative mt-2">
+        <FaSearch
+          size={20}
+          className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-300"
+        />
+        <input
+          type="search"
+          className="w-full focus:outline-none p-2 pl-10 text-sm dark:text-gray-900 rounded-lg"
+          name="search"
+          placeholder="Search Products..."
+        />
+      </div>
+      <Drawer
+        title="Cart"
+        placement="right"
+        closable={false}
+        onClose={onClose}
+        open={open}
+      >
+        {cartItems.map((cartItem, index) => (
+          <p key={index}>{cartItem.product.name}</p>
+        ))}
+      </Drawer>
+    </div>
+  );
+};
+
+export default Navbar;
--- resources/js/Pages/Front/Home.jsx
+++ resources/js/Pages/Front/Home.jsx
@@ -1,30 +1,33 @@
-import CategoryNavWithSlider from '@/Components/ui/CategoryNav'
-import ProductCard from '@/Components/ui/ProductCard'
-import FrontLayout from '@/Layouts/FrontLayout'
-import { Head, Link } from '@inertiajs/react'
+import CategoryNavWithSlider from "@/Components/ui/CategoryNav";
+import ProductCard from "@/Components/ui/ProductCard";
+import FrontLayout from "@/Layouts/FrontLayout";
+import { Head, Link } from "@inertiajs/react";
 
-const Home = ({ app, categories, products }) => {
-    return (
-        <FrontLayout>
-            <Head title="Home" />
-            <CategoryNavWithSlider categories={categories} />
-            <div className='text-2xl mt-8 text-orange-500 font-bold h-10 p-1 text-center'>
-                <h3>OUR PRODUCTS</h3>
-            </div>
-            <div className='grid grid-cols-1 md:grid-cols-6 gap-3 p-6'>
-                {products.data.map(product => (
-                    <Link href={route('view.product', product)} className='m-auto' key={product.id}>
-                        <ProductCard
-                            src={app.storage_url + '/' + product.image}
-                            name={product.name}
-                            price={"Rs. " + product.price}
-                        />
-                    </Link>
-                ))}
+const Home = ({ app, categories, products, cartItems }) => {
+  return (
+    <FrontLayout cartItems={cartItems}>
+      <Head title="Home" />
+      <CategoryNavWithSlider categories={categories} />
+      <div className="text-2xl mt-8 text-orange-500 font-bold h-10 p-1 text-center">
+        <h3>OUR PRODUCTS</h3>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-6 gap-3 p-6">
+        {products.data.map((product) => (
+          <Link
+            href={route("view.product", product)}
+            className="m-auto"
+            key={product.id}
+          >
+            <ProductCard
+              src={app.storage_url + "/" + product.image}
+              name={product.name}
+              price={"Rs. " + product.price}
+            />
+          </Link>
+        ))}
+      </div>
+    </FrontLayout>
+  );
+};
 
-            </div>
-        </FrontLayout>
-    )
-}
-
-export default Home
+export default Home;
