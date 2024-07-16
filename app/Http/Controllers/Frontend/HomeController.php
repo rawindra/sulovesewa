4c3fffc84e95187f621141c4722d2560b7b0806c
Bunny <bunnycdy@gmail.com>
2024-07-16 17:08:01 +0545

    order function added

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Cart;
 use App\Models\Category;
+use App\Models\Order;
+use App\Models\OrderItem;
 use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
+use Ramsey\Uuid\Codec\OrderedTimeCodec;
 
 class HomeController extends Controller
 {
@@ -56,4 +59,30 @@ class HomeController extends Controller
         }
         return redirect()->back();
     }
+
+    public function order()
+    {
+        $carts = Cart::where("user_id", auth()->user()->id)->get();
+        $order = new Order();
+        $order->order_date = now();
+        $order->order_status = 'pending';
+        $order->payment_status = 'unpaid';
+        $order->customer_name = auth()->user()->name;
+        $order->customer_contact_number = '9089161393';
+        $order->customer_address = '';
+        $order->order_note = '';
+        $order->save();
+        foreach ($carts as $cart) {
+            $orderItems = new OrderItem;
+            $orderItems->order_id = $order->id;
+            $orderItems->product_id = $cart->product_id;
+            $orderItems->quantity = $cart->quantity;
+            $orderItems->price = $cart->product->price * $cart->quantity;
+            $orderItems->save();
+        }
+        Cart::where("user_id", auth()->user()->id)->delete();
+
+        return redirect()->back();
+    }
+    
 }
