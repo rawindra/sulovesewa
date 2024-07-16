3898dbf2da272b843bfc3c800f6a82ab8d123a3c
Bunny <bunnycdy@gmail.com>
2024-07-16 17:07:48 +0545

    minor change

--- resources/js/Layouts/FrontLayout.jsx
+++ resources/js/Layouts/FrontLayout.jsx
@@ -2,7 +2,7 @@ import Footer from "@/Components/partials/Footer";
 import Navbar from "@/Components/partials/Navbar";
 import React from "react";
 
-const FrontLayout = ({ children, cartItems }) => {
+const FrontLayout = ({ app, children, cartItems }) => {
   return (
     <div className="overflow-x-hidden">
       <Navbar cartItems={cartItems} />
